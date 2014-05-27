a2ecdb0af06a2ed2d8253842018adfb5db0136f3
Stephen Billard <stephen@sbillard.org>
2014-05-26 18:54:25 -0700

    Federated logon examples

---
+++ plugins/federated_logon/Yahoo-demo_logon.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Yahoo accounts logon handler.
+ *
+ * This just supplies the Yahoo URL to OpenID_try.php. The rest is normal OpenID handling
+ *
+ * @author Stephen Billard (sbillard)
+ * @package plugins
+ * @subpackage users
+ */
+
+define('OFFSET_PATH', 3);
+require_once(dirname(dirname(dirname(__FILE__))).'/zp-core/admin-functions.php');
+require_once(SERVERPATH.'/'.ZENFOLDER.'/'.PLUGIN_FOLDER.'/federated_logon/consumer/common.php');
+session_start();
+
+if (isset($_GET['redirect'])) {
+	$redirect = sanitizeRedirect($_GET['redirect']);
+} else {
+	$redirect = '';
+}
+$_SESSION['OpenID_redirect'] = $redirect;
+//The following three lines are the provider dependent items
+$_SESSION['OpenID_cleaner_pattern'] = '/me.yahoo.com\/.*\/(.*)/';
+$_SESSION['provider'] = 'Yahoo-demo';
+$_GET['openid_identifier'] = 'https://Yahoo.com';
+
+// if the extension included with the standard federation login extensions
+// require 'consumer/try_auth.php';
+// otherwise we need to redirect to the consumer/try_auth.php script
+header('location:'.WEBPATH.'/'.ZENFOLDER.'/'.PLUGIN_FOLDER.'/federated_logon/consumer/try_auth.php?openid_identifier='.$_GET['openid_identifier'].'&action=verify');
+exitZP();
+?>
