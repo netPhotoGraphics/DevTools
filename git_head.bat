e1e9272587e7ba26d9e75695937ca5b430382faf
Stephen Billard <stephen@sbillard.org>
2014-08-21 13:43:17 -0700

    tool updates

--- git_head.bat
+++ git_head.bat
@@ -1,8 +1,5 @@
 @echo off
-# copyright Stephen Billard
-# permission is granted for use in conjunction with ZenPhoto20 all other rights reserved
 git rev-parse HEAD>zp-core\githead
 SET /P LONG=<zp-core\githead
 SET SHORT=%LONG:~0,10%
 git add zp-core\githead
-git commit -m"release id %SHORT%"
