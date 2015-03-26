ecf3263d03d7ff33239204a3911c9646dfdc1c2d
Stephen Billard <stephen@sbillard.org>
2015-03-26 10:07:19 -0700

    fix times with single digit hours

--- upstream.bat
+++ upstream.bat
@@ -1,18 +1,12 @@
 ECHO off
-FOR /f "tokens=1-4 delims=/ " %%i IN ("%date%") DO (
-	SET dow=%%i
-	SET month=%%j
-	SET day=%%k
-	SET year=%%l
+FOR /f "skip=1" %%x IN ('wmic os get localdatetime') DO IF NOT DEFINED mydate SET mydate=%%x
+FOR /f "tokens=1-3 delims=-." %%i IN ("%mydate%") DO (
+	SET p1=%%i
+	SET p2=%%j
+	SET p3=%%k
 )
-SET datestr=%month%_%day%_%year%
-FOR /f "tokens=1-3 delims=/:." %%i IN ("%time%") DO (
-	SET hr=%%i
-	SET min=%%j
-	SET sec=%%k
-)
-SET timestr=%hr%_%min%_%sec%
-SET upstream=%datestr%_%timestr%
+
+SET upstream=%p1%%p2%%p3%
 ECHO on
 
 git remote add %upstream% https://github.com/zenphoto/zenphoto.git
