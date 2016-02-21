31c2e977f2c376fe91545deda2ba67b492330299
Stephen Billard <stephen@sbillard.org>
2016-02-20 16:30:13 -0800

    creates database template from current database

---
+++ plugins/database_template.php
@@ -0,0 +1,38 @@
+<?php
+
+/* Creates the zenphoto database template file
+ *
+ * @author Stephen Billard (sbillard)
+ *
+ * @package plugins
+ * @subpackage development
+ * @category ZenPhoto20Tools
+ *
+ * Copyright Stephen L Billard
+ * permission granted for use in conjunction with ZenPhoto20. All other rights reserved
+ */
+// force UTF-8 Ø
+
+$plugin_is_filter = 5 | ADMIN_PLUGIN;
+$plugin_description = gettext('Generates the <em>zenphoto.package</em> file.');
+$plugin_author = "Stephen Billard (sbillard)";
+
+zp_register_filter('admin_utilities_buttons', 'database_structure_button');
+
+function database_structure_button($buttons) {
+	$buttons[] = array(
+					'category'		 => gettext('Development'),
+					'enable'			 => true,
+					'button_text'	 => gettext('Database Template'),
+					'formname'		 => 'database_template.php',
+					'action'			 => FULLWEBPATH . '/plugins/database_template/database.php',
+					'icon'				 => 'images/arrow_down.png',
+					'title'				 => gettext('Creates the database structure template.'),
+					'alt'					 => '',
+					'hidden'			 => '',
+					'rights'			 => ADMIN_RIGHTS
+	);
+	return $buttons;
+}
+
+?>
---
+++ plugins/database_template/database.php
@@ -0,0 +1,109 @@
+<?php
+/* Creates the zenphoto database template file
+ *
+ * @author Stephen Billard (sbillard)
+ *
+ * @package plugins
+ * @subpackage development
+ * @category ZenPhoto20Tools
+ *
+ * Copyright Stephen L Billard
+ * permission granted for use in conjunction with ZenPhoto20. All other rights reserved
+ */
+// force UTF-8 Ø
+
+define("OFFSET_PATH", 3);
+require_once(dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME']))) . "/zp-core/admin-globals.php");
+
+$zenphoto_tabs['overview']['subtabs'] = array(gettext('Database') => '');
+printAdminHeader('overview', 'Database');
+?>
+</head>
+<body>
+	<?php printLogoAndLinks(); ?>
+	<div id="main">
+		<?php printTabs(); ?>
+		<div id="content">
+			<?php printSubtabs() ?>
+			<div class="tabbox">
+				<?php zp_apply_filter('admin_note', 'database', ''); ?>
+				<h1><span id="top">Database Template</span></h1>
+				<?php
+				$database_name = db_name();
+				$prefix = trim(prefix(), '`');
+				$resource = db_show('tables');
+				if ($resource) {
+					$result = array();
+					while ($row = db_fetch_assoc($resource)) {
+						$result[] = $row;
+					}
+					db_free_result($resource);
+				} else {
+					$result = false;
+				}
+				$tables = array();
+				if (is_array($result)) {
+					foreach ($result as $row) {
+						$tables[] = array_shift($row);
+					}
+				}
+				$database = array();
+				$i = 0;
+				foreach ($tables as $table) {
+					$table = substr($table, strlen($prefix));
+
+					$tablecols = db_list_fields($table);
+					foreach ($tablecols as $key => $datum) {
+						if (strpos($datum['Comment'], 'optional_') === false) { // leave these for setup time decisions
+							//remove don't care fields
+							unset($datum['Collation']);
+							unset($datum['Key']);
+							unset($datum['Extra']);
+							unset($datum['Privileges']);
+							$database[$table]['fields'][$datum['Field']] = $datum;
+						}
+					}
+
+					$indices = array();
+					$sql = 'SHOW KEYS FROM ' . prefix($table);
+					$result = query_full_array($sql);
+					foreach ($result as $index) {
+						if ($index['Key_name'] !== 'PRIMARY') {
+							$index['Index_comment'] = 'zp20'; //flag as one of ours
+							$indices[$index['Key_name']][] = $index;
+						}
+					}
+					foreach ($indices as $keyname => $index) {
+						if (count($index) > 1) {
+							$column = array();
+							foreach ($index as $element) {
+								$column[] = "`" . $element['Column_name'] . "`";
+							}
+							$index = array_shift($index);
+							$index['Column_name'] = implode(',', $column);
+						} else {
+							$index = array_shift($index);
+							$index['Column_name'] = "`" . $index['Column_name'] . "`";
+						}
+						unset($index['Table']);
+						unset($index['Seq_in_index']);
+						unset($index['Collation']);
+						unset($index['Cardinality']);
+						unset($index['Comment']);
+
+						$database[$table]['keys'][$keyname] = $index;
+					}
+				}
+
+				file_put_contents(SERVERPATH . '/' . ZENFOLDER . '/databaseTemplate', serialize($database));
+
+				echo gettext("ZenPhoto20 database template created");
+				?>
+			</div>
+		</div><!-- content -->
+	</div><!-- main -->
+	<?php
+	printAdminFooter();
+	?>
+</body>
+</html>
