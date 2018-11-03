52244387e4299e976d713a82877bb98b6f1f89b7
stephen@sbillard.org <stephen@sbillard.org>
2018-11-03 11:14:19 -0700

    preserve uncompressed versions of some js files add extract file creation from github dev folder

--- createziparchive/createziparchive.php
+++ createziparchive/createziparchive.php
@@ -1,4 +1,5 @@
 <?php
+
 /* * *************************************************************
  *  Copyright notice
  *
@@ -29,21 +30,29 @@
  * 	Copyright 2014 by Stephen L Billard for use in {@link https://github.com/ZenPhoto20/netPhotoGraphics ZenPhoto20}
  * 	This copyright notice MUST APPEAR in all copies of the script!
  */
- Define('TARGET', 'package/');
- if (isset($_GET['source']) && $_GET['source']) {
-	 define('VARIENT',$_GET['source']);
- } else { 
-	 define('VARIENT','master');
- }
+Define('TARGET', 'package/');
+if (isset($_GET['source']) && $_GET['source']) {
+	define('VARIENT', $_GET['source']);
+} else {
+	define('VARIENT', 'master');
+}
 
 echo '<h1>Creating ' . VARIENT . ' extract zip file</h1>';
 $me = str_replace('\\', '/', $_SERVER['SCRIPT_NAME']);
 if (!isset($_GET['process'])) {
-	echo '<meta http-equiv="refresh" content="1; url=' . $me . '?process&source=' . VARIENT. '" />';
+	echo '<meta http-equiv="refresh" content="1; url=' . $me . '?process&source=' . VARIENT . '" />';
 	exit();
 }
 try {
-	$sourcefolder = '/Downloads/netPhotoGraphics' . '-' . VARIENT . '/'; 
+	switch (VARIENT) {
+		case 'DEV':
+		case 'master':
+			$sourcefolder = '/Downloads/netPhotoGraphics' . '-' . VARIENT . '/';
+			breal;
+		case 'GIT':
+			$sourcefolder = '/github/netPhotoGraphics-DEV/';
+			break;
+	}
 	require_once($sourcefolder . 'zp-core/version.php');
 	$targetname = TARGET . 'extract.php.bin';
 	$zipfilename = md5(time()) . 'extract.zip'; // replace with tempname()
@@ -106,99 +115,100 @@ function addFiles2Zip(ZipArchive $zip, $path, $removeFolder = false) {
 	}
 	closedir($d);
 }
+
 __HALT_COMPILER();<?php
 /*
- * 	This script is a derivitive work produced by createziparchive (c) 2008 iljitsch@mail.com cookiepattern.blogspot.com
- *
- * 	The derivitive work is copyright (c) 2014 by Stephen Billard, all rights reserved
- * 	This copyright notice must be included in all copies of this script.
- */
+* 	This script is a derivitive work produced by createziparchive (c) 2008 iljitsch@mail.com cookiepattern.blogspot.com
+*
+* 	The derivitive work is copyright (c) 2014 by Stephen Billard, all rights reserved
+* 	This copyright notice must be included in all copies of this script.
+*/
 Define('PHP_MIN_VERSION', '5.2');
 if (version_compare(PHP_VERSION, PHP_MIN_VERSION, '<')) {
-	die(sprintf(gettext('netPhotoGraphics requires PHP version %s or greater'), PHP_MIN_VERSION));
+die(sprintf(gettext('netPhotoGraphics requires PHP version %s or greater'), PHP_MIN_VERSION));
 }
 
 $me = str_replace('\\', '/', $_SERVER['SCRIPT_NAME']);
 echo "<h1>Extracting netPhotoGraphics _VERSION_ files</h1>";
 
 if (!isset($_GET['process'])) {
-	echo '<meta http-equiv="refresh" content="1; url=' . $me . '?process" />';
-	exit();
+echo '<meta http-equiv="refresh" content="1; url=' . $me . '?process" />';
+exit();
 }
 $const_webpath = "http://" . $_SERVER['HTTP_HOST'] . rtrim(dirname($me), '/\\');
 
 try {
-	$zipfilename = md5(time()) . '.extract.zip'; //remove with tempname()
-	$fp_tmp = fopen($zipfilename, 'w');
-	$fp_cur = fopen(__FILE__, 'r');
-	fseek($fp_cur, __COMPILER_HALT_OFFSET__);
-	$i = 0;
-	while ($buffer = fread($fp_cur, 10240)) {
-		fwrite($fp_tmp, $buffer);
-	}
-	fclose($fp_cur);
-	fclose($fp_tmp);
-	$zipfile = new ZipArchive();
-	if (($result = $zipfile->open($zipfilename)) === true) {
-		set_time_limit(360);
-		if (!$zipfile->extractTo('.')) {
-			$error = error_get_last();
-			throw new Exception($error['message'], 0);
-		}
-	} else {
-		switch ($result) {
-			case ZipArchive::ER_INCONS:
-				$msg = 'Inconsistent archive';
-				break;
-			case ZipArchive::ER_MEMORY:
-				$msg = 'Insufficient memory';
-				break;
-			case ZipArchive::ER_NOENT:
-				$msg = 'File not found';
-				break;
-			case ZipArchive::ER_NOZIP:
-				$msg = 'Not a zip archive';
-				break;
-			case ZipArchive::ER_OPEN:
-				$msg = "Can't open file";
-				break;
-			case ZipArchive::ER_READ:
-				$msg = 'Read error';
-				break;
-			case ZipArchive::ER_SEEK:
-				$msg = 'Seek error';
-				break;
-			default:
-				$msg = 'Error ' . $result;
-				break;
-		}
-		throw new Exception('reading archive failed: ' . $msg, 1);
-	}
-	$zipfile->close();
-	unlink($zipfilename);
-	unlink(__FILE__);
-	?>
-	done...
-	<br />
-	<a href="<?php echo $const_webpath . '/zp-core/setup/index.php?autorun=admin'; ?>">run setup</a>
+$zipfilename = md5(time()) . '.extract.zip'; //remove with tempname()
+$fp_tmp = fopen($zipfilename, 'w');
+$fp_cur = fopen(__FILE__, 'r');
+fseek($fp_cur, __COMPILER_HALT_OFFSET__);
+$i = 0;
+while ($buffer = fread($fp_cur, 10240)) {
+fwrite($fp_tmp, $buffer);
+}
+fclose($fp_cur);
+fclose($fp_tmp);
+$zipfile = new ZipArchive();
+if (($result = $zipfile->open($zipfilename)) === true) {
+set_time_limit(360);
+if (!$zipfile->extractTo('.')) {
+$error = error_get_last();
+throw new Exception($error['message'], 0);
+}
+} else {
+switch ($result) {
+case ZipArchive::ER_INCONS:
+$msg = 'Inconsistent archive';
+break;
+case ZipArchive::ER_MEMORY:
+$msg = 'Insufficient memory';
+break;
+case ZipArchive::ER_NOENT:
+$msg = 'File not found';
+break;
+case ZipArchive::ER_NOZIP:
+$msg = 'Not a zip archive';
+break;
+case ZipArchive::ER_OPEN:
+$msg = "Can't open file";
+break;
+case ZipArchive::ER_READ:
+$msg = 'Read error';
+break;
+case ZipArchive::ER_SEEK:
+$msg = 'Seek error';
+break;
+default:
+$msg = 'Error ' . $result;
+break;
+}
+throw new Exception('reading archive failed: ' . $msg, 1);
+}
+$zipfile->close();
+unlink($zipfilename);
+unlink(__FILE__);
+?>
+done...
+<br />
+<a href="<?php echo $const_webpath . '/zp-core/setup/index.php?autorun=admin'; ?>">run setup</a>
 
-	<script>
-		// <!-- <![CDATA[
-		window.onload = function () {
-			window.location = '<?php echo $const_webpath; ?>/zp-core/setup/index.php?autorun=admin';
-		}
-		// ]]> -->
-	</script>
-	<?php
-} catch (Exception $e) {
-	$zipfile->close();
-	@unlink($zipfilename);
-	echo "Error:<br />";
-	echo $e->getMessage() . "<br />";
-	if ($e->getCode()) {
-		echo $e->getTraceAsString();
-	} else {
-		echo "Try removing the old installation files.";
+<script>
+	// <!-- <![CDATA[
+	window.onload = function () {
+		window.location = '<?php echo $const_webpath; ?>/zp-core/setup/index.php?autorun=admin';
 	}
+	// ]]> -->
+</script>
+<?php
+} catch (Exception $e) {
+$zipfile->close();
+@unlink($zipfilename);
+echo "Error:<br />";
+echo $e->getMessage() . "<br />";
+if ($e->getCode()) {
+echo $e->getTraceAsString();
+} else {
+echo "Try removing the old installation files.";
+}
 }
 __HALT_COMPILER();
