b30179cb82d816931f090d7c794a9b1a627c7ff3
stephen@sbillard.org <stephen@sbillard.org>
2018-11-04 15:24:33 -0800

    exclude the batch files from the package

--- createziparchive/createziparchive.php
+++ createziparchive/createziparchive.php
@@ -103,7 +103,7 @@ function addFiles2Zip(ZipArchive $zip, $path, $removeFolder = false) {
 	$d = opendir($path);
 	while ($file = readdir($d)) {
 		set_time_limit(360);
-		if ($file{0} == "." || $file == 'Thumbs.db' || getSuffix($file) == 'md')
+		if ($file{0} == "." || $file == 'Thumbs.db' || ($sfx = getSuffix($file)) == 'md' || $sfx == 'bat')
 			continue;
 		$curfile = ($removeFolder) ? preg_replace('~^' . $removeFolder . '~', '', $path . $file) : $path . $file;
 		if (is_dir($path . $file)) {
