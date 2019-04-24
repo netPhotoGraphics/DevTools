b6f823dd30a92fc018810be10afabd3c85bc437e
stephen@sbillard.org <stephen@sbillard.org>
2019-04-24 15:16:35 -0700

    suppress error messages from the copy function

--- plugins/devReleases.php
+++ plugins/devReleases.php
@@ -1,5 +1,4 @@
 <?php
-
 /* Provides an install button for development releases
  *
  * This plugin is will place an install button pointing to the current Development
@@ -48,53 +47,63 @@ if (class_exists('Milo\Github\Api') && zpFunctions::hasPrimaryScripts()) {
 	$devVersion = preg_replace('~[^0-9,.]~', '', str_replace('setup-', '', stripSuffix(basename($devVersionURI))));
 	$zenphoto_version = explode('-', ZENPHOTO_VERSION);
 	$zenphoto_version = preg_replace('~[^0-9,.]~', '', array_shift($zenphoto_version));
-	if (version_compare($devVersion, $zenphoto_version, '>')) {
-		zp_register_filter('admin_utilities_buttons', 'devRelease::buttons');
+
+	zp_register_filter('admin_utilities_buttons', 'devRelease::buttons');
+	if (isset($_GET['update_check'])) {
+		zp_register_filter('admin_note', 'devRelease::notice');
 	}
 }
-if (isset($_GET['action']) && $_GET['action'] == 'install_dev') {
-	XSRFdefender('install_update');
-	$msg = FALSE;
-
-	@ini_set('allow_url_fopen', 1);
-	if (!(($fopen = ini_get('allow_url_fopen')) && copy($devVersionURI, SERVERPATH . '/' . basename($devVersionURI)))) {
-		$class = 'errorbox';
-		$msg = gettext('netPhotoGraphics could not download the update.');
-		if (!$fopen) {
-			$msg .= ' ' . gettext('<em>allow_url_fopen</em> is not enabled in your PHP.ini configuration file. ');
-		}
-	}
-	if (!$msg) {
-		$found = safe_glob(SERVERPATH . '/setup-*.zip');
-		if (!empty($found)) {
-			$file = array_shift($found);
-			if (!unzip($file, SERVERPATH)) {
-				$class = 'errorbox';
-				$msg = gettext('netPhotoGraphics could not extract extract.php.bin from zip file.');
-			} else {
-				unlink(SERVERPATH . '/readme.txt');
-				unlink(SERVERPATH . '/release notes.htm');
-			}
-		}
-		if (file_exists(SERVERPATH . '/extract.php.bin')) {
-			if (isset($file)) {
-				unlink($file);
-			}
-			if (rename(SERVERPATH . '/extract.php.bin', SERVERPATH . '/extract.php')) {
-				header('Location: ' . FULLWEBPATH . '/extract.php');
-				exit();
-			} else {
-				$class = 'errorbox';
-				$msg = gettext('Renaming the <code>extract.php.bin</code> file failed.');
-			}
-		} else {
-			$class = 'errorbox';
-			$msg = gettext('Did not find the <code>extract.php.bin</code> file.');
-		}
-	}
-	if ($msg) {
-		header('location: ' . FULLWEBPATH . '/' . ZENFOLDER . '/admin.php?action=external&error&msg=' . html_encode($msg));
+if (isset($_GET['action'])) {
+	if ($_GET['action'] == 'check_updates') {
+		XSRFdefender('check_update');
+		purgeOption('getDEVUpdates_lastCheck');
+		purgeOption('getUpdates_lastCheck');
+		header('location: ' . FULLWEBPATH . '/' . ZENFOLDER . '/admin.php?update_check');
 		exit();
+	}
+	if ($_GET['action'] == 'install_dev') {
+
+		$msg = FALSE;
+		@ini_set('allow_url_fopen', 1);
+		if (!(($fopen = ini_get('allow_url_fopen')) && @copy($devVersionURI, SERVERPATH . '/' . basename($devVersionURI)))) {
+			$class = 'errorbox';
+			$msg = gettext('netPhotoGraphics could not download the update.');
+			if (!$fopen) {
+				$msg .= ' ' . gettext('<em>allow_url_fopen</em> is not enabled in your PHP.ini configuration file. ');
+			}
+		}
+		if (!$msg) {
+			$found = safe_glob(SERVERPATH . '/setup-*.zip');
+			if (!empty($found)) {
+				$file = array_shift($found);
+				if (!unzip($file, SERVERPATH)) {
+					$class = 'errorbox';
+					$msg = gettext('netPhotoGraphics could not extract extract.php.bin from zip file.');
+				} else {
+					unlink(SERVERPATH . '/readme.txt');
+					unlink(SERVERPATH . '/release notes.htm');
+				}
+			}
+			if (file_exists(SERVERPATH . '/extract.php.bin')) {
+				if (isset($file)) {
+					unlink($file);
+				}
+				if (rename(SERVERPATH . '/extract.php.bin', SERVERPATH . '/extract.php')) {
+					header('Location: ' . FULLWEBPATH . '/extract.php');
+					exit();
+				} else {
+					$class = 'errorbox';
+					$msg = gettext('Renaming the <code>extract.php.bin</code> file failed.');
+				}
+			} else {
+				$class = 'errorbox';
+				$msg = gettext('Did not find the <code>extract.php.bin</code> file.');
+			}
+		}
+		if ($msg) {
+			header('location: ' . FULLWEBPATH . '/' . ZENFOLDER . '/admin.php?action=external&error&msg=' . html_encode($msg));
+			exit();
+		}
 	}
 }
 
@@ -107,24 +116,61 @@ class devRelease {
 	}
 
 	static function buttons($buttons) {
-		global $devVersion;
-		$buttons[] = array(
-				'XSRFTag' => 'install_update',
-				'category' => gettext('Updates'),
-				'enable' => 2,
-				'button_text' => sprintf(gettext('Install DEV %1$s'), $devVersion),
-				'formname' => 'download_update',
-				'action' => FULLWEBPATH . '/' . ZENFOLDER . '/admin.php?action=install_dev',
-				'icon' => INSTALL,
-				'alt' => '',
-				'title' => sprintf(gettext('Download and install netPhotoGraphics development version %1$s on your site.'), $devVersion),
-				'hidden' => '<input type="hidden" name="action" value="install_dev" />',
-				'rights' => ADMIN_RIGHTS
-		);
-
+		global $devVersion, $zenphoto_version;
+		if (version_compare($devVersion, $zenphoto_version, '>')) {
+			$buttons[] = array(
+					'XSRFTag' => 'install_update',
+					'category' => gettext('Updates'),
+					'enable' => 2,
+					'button_text' => sprintf(gettext('Install DEV %1$s'), $devVersion),
+					'formname' => 'download_update',
+					'action' => FULLWEBPATH . '/' . ZENFOLDER . '/admin.php?action=install_dev',
+					'icon' => INSTALL,
+					'alt' => '',
+					'title' => sprintf(gettext('Download and install netPhotoGraphics development version %1$s on your site.'), $devVersion),
+					'hidden' => '<input type="hidden" name="action" value="install_dev" />',
+					'rights' => ADMIN_RIGHTS
+			);
+		} else {
+			$check = true;
+			foreach ($buttons as $button) {
+				if (isset($button['category']) && $button['category'] == gettext('Updates')) {
+					$check = false;
+					break;
+				}
+			}
+			if ($check) {
+				$buttons[] = array(
+						'XSRFTag' => 'check_update',
+						'category' => gettext('Updates'),
+						'enable' => 1,
+						'button_text' => gettext('Check for updates'),
+						'formname' => 'check_update',
+						'action' => FULLWEBPATH . '/' . ZENFOLDER . '/admin.php?action=check_updates',
+						'icon' => CLOCKWISE_OPEN_CIRCLE_ARROW_GREEN,
+						'alt' => '',
+						'title' => gettext('Check for newer versions of netPhotoGraphics.'),
+						'hidden' => '<input type="hidden" name="action" value="check_updates" />',
+						'rights' => ADMIN_RIGHTS
+				);
+			}
+		}
 		return $buttons;
 	}
 
-}
+	static function notice() {
+		global $devVersion, $zenphoto_version;
+		if (isset($newestVersion) || version_compare($devVersion, $zenphoto_version, '>')) {
+			$msg = gettext('There is an update available.');
+		} else {
+			$msg = gettext('You are running the latest netPhotoGraphics version.');
+		}
+		?>
+		<div class="messagebox fade-message">
+			<h2><?php echo $msg; ?></h2>
+		</div>
+		<?php
+	}
 
+}
 ?>
