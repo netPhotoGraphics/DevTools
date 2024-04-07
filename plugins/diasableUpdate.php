46df2bc8132ea755cbc49336a99ebfe4446a092d
Stephen Billard <stephen@sbillard.org>
2024-04-07 12:16:46 -0700

    replace INSTALL icon with PROHIBITED icon for visual reminder

--- plugins/diasableUpdate.php
+++ plugins/diasableUpdate.php
@@ -15,18 +15,23 @@ npgFilters::register('admin_close', 'DisableUpdate');
 function DisableUpdate() {
 	?>
 	<script type="text/javascript">
+		$('#download_update .font_icon').html('<?php echo PROHIBITED; ?>');
 		$('#download_update').submit(function (e) {
 			if (!confirm('<?php echo gettext('Do you really want to download and install this release?'); ?>')) {
 				e.preventDefault(e);
 			}
 		}
 		)
+
+
+		$('#download_Dev_update .font_icon').html('<?php echo PROHIBITED; ?>');
 		$('#download_Dev_update').submit(function (e) {
 			if (!confirm('<?php echo gettext('Do you really want to download and install this Development release?'); ?>')) {
 				e.preventDefault(e);
 			}
 		}
 		)
+		$('#install_update .font_icon').html('<?php echo PROHIBITED; ?>');
 		$('#install_update').submit(function (e) {
 			if (!confirm('<?php echo gettext('Do you really want to iistall this release and overwrite this project?'); ?>')) {
 				e.preventDefault(e);
