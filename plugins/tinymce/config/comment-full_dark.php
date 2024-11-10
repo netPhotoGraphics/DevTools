7d0112112b55d213450c8d7144caee336f9b14c2
Stephen Billard <stephen@sbillard.org>
2024-11-10 14:24:00 -0800

    tinyMCE config examploes

---
+++ plugins/tinymce/config/comment-full_dark.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$MCEskin = "oxide-dark";
+$MCEcss = "dark";
+include(TINYMCE . '/config/comment-full.php');
