2459fc2101ad8d21065930390632c8b69afa39c3
Stephen Billard <stephen@sbillard.org>
2025-10-17 15:57:47 -0700

    user guide batch file

--- user_guide.bat
+++ user_guide.bat
@@ -2,8 +2,10 @@
 
 rem update the user guide
 D:\github\DevTools\docto -f "D:\github\DevTools-DEV\user guide.docx" -O "docs/user guide.pdf" -T wdFormatPDF
+del D:\test-sites\dev\docs\user guide.pdf
+copy docs/user guide.pdf D:\test-sites\dev\docs\
 
 rem commit the changes
 
 @git add .
-@git commit -m"release notes"
+@git commit -m"user guide"
